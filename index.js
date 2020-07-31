c841029159ff432d1181d1aacc98f6c079da4ab0
Matthew Ogtong <ogtongm@gmail.com>
2020-07-31 03:06:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musician, instrument) {
+  let array = []
+  for ( let i = 0; musician.length ; i++) {
+    array.push(`${musician} plays ${instrument}.`)
+  }
+  return array
+}
